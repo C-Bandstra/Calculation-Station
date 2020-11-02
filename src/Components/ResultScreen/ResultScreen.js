75c11d340cd8de6cdf9d82d022838db1c143935a
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-01 17:34:39 -0700

    Create Result Screen Component

---
+++ src/Components/ResultScreen/ResultScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ResultScreen.css';
+
+const ResultScreen = ({ shadowResult, result, currentSequence }) => {
+
+  return (
+    <div className="result-screen">
+      <p  className={`input-display ${result ? "hidden" : ""}`}>{currentSequence}</p>
+      <p  className={`shadow-result ${result ? "hidden" : ""}`}>{shadowResult}</p>
+      <p  className={`result ${result ? "" : "hidden"}`}>{result}</p>
+    </div>
+  )
+}
+
+export default ResultScreen
