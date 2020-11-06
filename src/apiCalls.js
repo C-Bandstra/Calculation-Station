9be77b0645e59e478f04a0e0cfc83a8b4d8ba5b9
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-06 14:58:06 -0700

    Add fetch request methods

---
+++ src/apiCalls.js
@@ -0,0 +1,26 @@
+export const fetchCalculations = async () => {
+  const response = await fetch('https://calculation-station.herokuapp.com/calculations')
+    const data = await response.json();
+    return data;
+}
+
+export const addCalculation = async (calculation) => {
+  calculation = {"sequence": calculation}
+  try {
+    const response = await fetch(
+      "https://calculation-station.herokuapp.com/addCalculation",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(calculation)
+      }
+    );
+    const calculationsResponse = await response.json(calculation);
+    console.log(calculationsResponse)
+    return calculationsResponse;
+  } catch (error) {
+    console.error(error);
+  }
+};
