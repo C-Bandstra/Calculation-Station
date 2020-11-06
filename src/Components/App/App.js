b79f98e8da2cb3c8c8cd891040d5a1af132eaf9e
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-06 14:58:56 -0700

    Convert to class component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CalculationBoard from '../CalculationBoard/CalculationBoard';
 import Calculator from "../Calculator/Calculator"
+import { fetchCalculations } from '../../apiCalls'
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Calculation Station</h1>
-      <Calculator/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fetchedCalculations: [],
+    }
+  }
+  
+
+  componentDidMount = async () => {
+    const calculations = await fetchCalculations()
+    this.setState({
+      fetchedCalculations: calculations
+    })
+  }
+
+  updateCalculations = (updatedCalculations) => {
+    this.setState({
+      fetchedCalculations: updatedCalculations.calculations
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {/* <h1>Calculation Station</h1> */}
+        <section className="components">
+          <CalculationBoard currentState={this.state}/>
+          <Calculator updateCalculations={this.updateCalculations}/>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
