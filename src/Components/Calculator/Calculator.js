3070ffbd27044142489878e3026c8ab23096a159
Charlie Bandstra <bandstracharlie@gmail.com>
2020-11-06 15:00:10 -0700

    Add fetch error handling methods

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,18 +1,18 @@
-//CALCULATION ERROR OCCURS WHEN INPUTING MULTIPLE OPERATORS IN ONE SEQUENCE,
+//CALCULATION ERROR OCCURS WHEN INPUTING 2 OR MORE SIMILAR OPERATORS IN ONE SEQUENCE,
 //THIS IS BECAUSE THE INDEXOF() IS FINDING THE FIRST CHARACTER MATCHING INSTEAD OF BOTH
-
-
+//Ref: Line 61
 
 import React, { Component } from 'react';
 import './Calculator.css';
 import Keyboard from "../Keyboard/Keyboard"
 import ResultScreen from "../ResultScreen/ResultScreen"
+import { addCalculation } from '../../apiCalls'
 
 const Parser = require('expr-eval').Parser;
 
 class Calculator extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       currentSequence: '',
       result: '',
@@ -34,11 +34,19 @@ class Calculator extends Component {
     return result
   }
 
+  beautifySequence = (sequence) => {
+    const beautifiedSequence = sequence.split('').join(' ');
+    return beautifiedSequence
+  }
+
   calculate = () => {
     let sequence = this.state.currentSequence
     sequence = this.replaceChar(sequence, 'x', '*')
     sequence = this.replaceChar(sequence, '÷', '/')
     let result = Parser.evaluate(sequence)
+    if(result == 'Infinity') {
+      result = 0
+    }
     result = this.beautifyResult(result)
     return result
   }
@@ -58,19 +66,36 @@ class Calculator extends Component {
     }
   }
 
-  storeKey = (clickedKey) => {
+  sendCalculation = async (result, sequence) => {
+    sequence = this.beautifySequence(sequence)
+    const calculation = `${sequence} = ${result}`
+    const calculations = await addCalculation(calculation)
+    return calculations
+  }
+
+  storeKey = (clickedKey, symbol) => {
     let currentState = this.state.currentSequence
-    let updatedState = currentState += clickedKey
+    let updatedState = this.checkSequence(currentState, clickedKey, symbol)
     this.setState({
       currentSequence: updatedState
     })
   }
 
-  storeResult = (result) => {
+  checkSequence = (currentState, clickedKey, symbol) => {
+    if(symbol || this.state.result != this.state.currentSequence) {
+      return currentState += clickedKey
+    } else {
+      return clickedKey
+    }
+  }
+
+  storeResult = (result, sequence) => {
     this.setState({
       currentSequence: result,
       result: result
     })
+    const calculations = this.sendCalculation(result, sequence)
+    return calculations
   }
 
   updateShadow = (shadowResult) => {
@@ -83,7 +108,7 @@ class Calculator extends Component {
     return (
       <div className="calculator">
         <ResultScreen shadowResult={this.state.shadowResult} result={this.state.result} currentSequence={this.state.currentSequence} />
-        <Keyboard clearResult={this.clearResult} updateShadow={this.updateShadow} currentSequence={this.state.currentSequence} storeResult={this.storeResult} calculate={this.calculate} allClear={this.allClear} storeKey={this.storeKey} />
+        <Keyboard updateCalculations={this.props.updateCalculations} clearResult={this.clearResult} updateShadow={this.updateShadow} currentSequence={this.state.currentSequence} storeResult={this.storeResult} calculate={this.calculate} allClear={this.allClear} storeKey={this.storeKey} />
       </div>
     )
   }
